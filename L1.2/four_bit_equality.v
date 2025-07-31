8567226727aca08f6fd64ce434e19b56e1e4abcf
Advaith Giridhar <95209599+weaselchokingonachickenbone69@users.noreply.github.com>
2025-07-31 13:58:22 +0530

    Update four_bit_equality.v to make it a functional implementation

--- L1.2/four_bit_equality.v
+++ L1.2/four_bit_equality.v
@@ -3,5 +3,5 @@ module four_bit_equality (
     input [3:0] B,
     output ans
 );
-    assign ans = (A == B);
+    assign ans = ~(A[0] ^ B[0]) & ~(A[1] ^ B[1]) & ~(A[2] ^ B[2]) & ~(A[3] ^ B[3]);
 endmodule
